802a1297bc1fedb393bdb5d9198cc79beb67cfd8
jeeboeira <132774888+jeeboeira@users.noreply.github.com>
2023-05-29 19:59:16 -0300

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+from time import gmtime, strftime
+
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('DB-AWS-DIO-APISecurity')
+now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+
+
+def lambda_handler(event, context):
+    name = event['firstName'] +' '+ event['lastName']
+    response = table.put_item(
+        Item={
+            'ID': name,
+            'LatestGreetingTime':now
+            })
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Your name was insert with success, ' + name)
+    }
